db2dc63c08d2622342ea5c64a8417077dd5046ed
Ferdinant2000 <firdavskomolitdinov@gmail.com>
2025-07-09 21:45:24 +0500

    Fixed the problem

--- pages/CardsTab/interactive-slider/SecurityLevelSlider.tsx
+++ pages/CardsTab/interactive-slider/SecurityLevelSlider.tsx
@@ -28,7 +28,7 @@ export default function App() {
                     <div className="text-center p-6 rounded-lg transition-all duration-300">
                         <div className="flex justify-center mb-4">
                             <div className="text-danger-500">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="w-6 h-6">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-6 h-6">
                                     <path d="m21.73 18-8-14a2 2 0 0 0-3.48 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3Z"></path>
                                     <path d="M12 9v4"></path>
                                     <path d="M12 17h.01"></path>
